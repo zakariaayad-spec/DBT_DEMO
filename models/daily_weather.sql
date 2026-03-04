1895fe959696fea5b60d5d8c4b78f80ea8c803c4
Zakaria Ayad <zakaria.ayad@kodemade.com>
2026-03-04 14:39:48 +0000

    daily weather created

--- models/daily_weather.sql
+++ models/daily_weather.sql
@@ -1,8 +1,11 @@
+{{ config(materialized='table') }}
+
 WITH daily_weather as (
 
 select 
     date(TIME) as daily_weather,
     weather,
+    temp,
     pressure,
     humidity,
     clouds
@@ -12,11 +15,33 @@ from
 daily_weather_agg as (
 select 
     daily_weather, weather,
-    count(weather) as count_weather
+    count(weather) as count_weather,
+    ROW_NUMBER() over (PARTITION by daily_weather order by count(weather) desc ) as row_number
 from
     daily_weather
 group by 
     daily_weather, weather
-)
+), result as (
 
-select * from  daily_weather_agg 
+select daily_weather.daily_weather
+, daily_weather.weather , daily_weather.temp, 
+daily_weather.pressure,
+daily_weather.humidity,
+daily_weather.clouds
+--, daily_weather.pressure
+
+ from  daily_weather 
+left join daily_weather_agg
+on daily_weather_agg.daily_weather=daily_weather.daily_weather and 
+daily_weather_agg.weather=daily_weather.weather
+where row_number=1 )
+
+select result.daily_weather
+, result.weather , round(avg(result.temp),2) as avg_temp ,
+round(avg(result.pressure),2) as avg_pressure,
+round(avg(result.humidity),2) as avg_humidity,
+round(avg(result.clouds),2) as avg_clouds from result
+group by result.daily_weather
+, result.weather
+order by daily_weather asc 
+
